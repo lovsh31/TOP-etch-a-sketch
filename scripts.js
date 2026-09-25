69221244fea74ebc3e50bec55d8cdffba888e89a
lovsh31 <283480261+lovsh31@users.noreply.github.com>
2026-09-25 17:21:43 +0200

    Add progressive opacity and randomize colors
    - Each square becomes darker as it is hovered
    - Square color is randomized on creation
    - Draw only while the mouse is DOWN

--- scripts.js
+++ scripts.js
@@ -3,7 +3,7 @@ It’s best to put your grid squares inside a “container” div. This div can 
 Use Flexbox to make the divs appear as a grid (versus just one on each line). Despite the name, do not be tempted to research or use CSS Grid, as it will be taught in a later lesson after the foundations path. This project is an opportunity specifically to practice Flexbox!*/
 const container = document.querySelector("#container");
 const buttonNewGrid = document.querySelector("#newGrid");
-buttonNewGrid.textContent = "Change Size";
+buttonNewGrid.textContent = "Change Grid Size";
 let squareNumber = 16;
 let squareColor = "brown";
 
@@ -25,12 +25,48 @@ function createGrid(size){
         let divGrid = document.createElement("div");
         divGrid.classList.add("square");
         divGrid.style.width=(100/size)+"%"; //Adjust the size of each square to fit 16 per line
+
+        //color version
         let randomColor = "#"+Math.floor(Math.random()*1000000); 
-        divGrid.style.setProperty('--hoverColor', randomColor); //Set a randomColor to the CSS variable --hoverColor
+        divGrid.style.backgroundColor = randomColor;
+
+        //B&W
+        //divGrid.style.backgroundColor = "black";
+
         container.appendChild(divGrid);
-    }    
+
+        
+        let opacity = Number(divGrid.style.opacity);
+        let isMouseDown = false;
+
+        function changeOpacity(){ //darken the square by 20% on each hover
+            if(isMouseDown){
+                if(opacity<1)
+                    opacity = opacity + 0.2;
+                divGrid.style.opacity = opacity;
+            }
+        }
+
+        container.addEventListener("mousedown", (e)=>{
+            e.preventDefault()
+            isMouseDown = true;
+        });
+        container.addEventListener("mouseup", ()=>{
+            isMouseDown = false;
+
+            //divGrid.removeEventListener("mouseover", changeColor);
+        });
+        
+        
+        divGrid.addEventListener("mouseover", changeOpacity);
+
+     }    
     
 }
 
 
 createGrid(squareNumber);
+
+/*
+divGrid.style.setProperty('--hoverColor', randomColor); //Set a randomColor to the CSS variable --hoverColor
+divGrid.style.backgroundColor = randomColor;*/
