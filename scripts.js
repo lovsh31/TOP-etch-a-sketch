def1c5b2bb537dc9dfbc097ea6a28c10ea07e6dd
lovsh31 <283480261+lovsh31@users.noreply.github.com>
2026-09-25 23:21:00 +0200

    Add grid and clear
    - Add a button to show or hide the grid
    - Add a button to clear the board while keeping
    the grid number and color
    - Adjust general style

--- scripts.js
+++ scripts.js
@@ -1,32 +1,37 @@
-/*Create the divs using JavaScript. Don’t try to create them by hand by copying and pasting them in your HTML file!
-It’s best to put your grid squares inside a “container” div. This div can be written in your HTML file.
-Use Flexbox to make the divs appear as a grid (versus just one on each line). Despite the name, do not be tempted to research or use CSS Grid, as it will be taught in a later lesson after the foundations path. This project is an opportunity specifically to practice Flexbox!*/
 const container = document.querySelector("#container");
 const buttonNewGrid = document.querySelector("#newGrid");
+const buttonShowGrid = document.querySelector("#showGrid");
+const buttonClear = document.querySelector("#clearGrid");
 const selectColorBrush = document.querySelector("#colors");
 buttonNewGrid.textContent = "Change Grid Size";
+buttonClear.textContent = "Clear";
 
-const colorArr = ["black"];
-selectColorBrush.style.backgroundColor = "black";
+//Set Black as default and create a list of colors
+const colorArr = ["0, 100%, 0%"];
+selectColorBrush.style.backgroundColor = "hsl(0, 100%, 0%)";
+selectColorBrush.value = "0, 100%, 0%";
 
 for (let i = 0; i < 360; i+=20) {
-  colorArr.push(`hsl(${i}, 100%, 50%)`);
+  colorArr.push(i+", 100%, 50%"); //create a rainbow of colors
 }
 for (let color in colorArr){
     let option = document.createElement("option");
     option.value = colorArr[color];
-    option.style.backgroundColor = colorArr[color];
+    option.style.backgroundColor = "hsl("+colorArr[color]+")";
     selectColorBrush.add(option);
 }
 
+//Set base squares numbers to 16
 let squareNumber = 16;
-
 buttonNewGrid.addEventListener("click", ()=>{
     squareNumber = Number(prompt("Enter squares Number"));
     if(!Number.isInteger(squareNumber))
         createGrid(16);
     else
         createGrid(squareNumber);    
+});
+buttonClear.addEventListener("click",()=>{
+    createGrid(squareNumber);
 });
 
 
@@ -40,49 +45,62 @@ function createGrid(size){
         divGrid.classList.add("square");
         divGrid.style.width=(100/size)+"%"; //Adjust the size of each square to fit 16 per line
 
-        //color version
-        let randomColor = "#"+Math.floor(Math.random()*1000000); 
-        divGrid.style.backgroundColor = randomColor;
-
         //B&W
-        divGrid.style.backgroundColor = "black";
+        divGrid.style.backgroundColor = `hsl(${selectColorBrush.value}, 0)`;
 
         container.appendChild(divGrid);
 
         
-        let opacity = Number(divGrid.style.opacity);
+        let opacity = 0;
         let isMouseDown = false;
-        function changeColor(){
-            selectColorBrush.style.backgroundColor = selectColorBrush.value;
-        }
+
         function changeOpacity(){ //darken the square by 20% on each hover
             if(isMouseDown){
-                divGrid.style.backgroundColor = selectColorBrush.value;
+                divGrid.style.backgroundColor = `hsl(${selectColorBrush.value}, ${opacity})`; //use the selected color
                 if(opacity<1)
                     opacity = opacity + 0.2;
-                divGrid.style.opacity = opacity;
+                divGrid.style.backgroundColor = `hsl(${selectColorBrush.value}, ${opacity})`;
             }
         }
-        selectColorBrush.addEventListener("change", changeColor)
+        selectColorBrush.addEventListener("change", ()=>{ //Change color of the select  
+            selectColorBrush.style.backgroundColor = `hsl(${selectColorBrush.value})`;
+        });        
 
+        //Test if mouse is down while listening to the squares 
         container.addEventListener("mousedown", (e)=>{
             e.preventDefault()
             isMouseDown = true;
         });
+
         window.addEventListener("mouseup", ()=>{
             isMouseDown = false;
         });
-        
+
+        divGrid.addEventListener("mousedown", (e)=>{ //Trigger changeOpacity on click
+            e.preventDefault()
+            isMouseDown = true;
+            changeOpacity();
+        });   
         
         divGrid.addEventListener("mouseover", changeOpacity);
 
-     }    
+        let gridOn = true;
+        buttonShowGrid.textContent="Hide grid";
+        divGrid.style.border = "1px solid hsl(0, 0%, 94%)";
+        buttonShowGrid.addEventListener("click", ()=>{
+            if (!gridOn){
+                divGrid.style.border = "1px solid hsl(0, 0%, 94%)";
+                buttonShowGrid.textContent="Hide grid";
+                gridOn = true;
+            }else{
+                divGrid.style.border = "0px";
+                buttonShowGrid.textContent="Show grid";
+                gridOn = false;
+            }
+        });
+        
+    }    
     
 }
 
-
 createGrid(squareNumber);
-
-/*
-divGrid.style.setProperty('--hoverColor', randomColor); //Set a randomColor to the CSS variable --hoverColor
-divGrid.style.backgroundColor = randomColor;*/
